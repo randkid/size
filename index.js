b584addd52cdabf66af343191de16a2c45cbd2e8
Gnlow <entrydark@gmail.com>
2020-05-09 10:29:10 +0900

    0.2.1

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 module.exports = {
     m: require("./m.js"),
-    f: require("./f.js")
+    f: require("./f.js"),
+    m_dropna: require("./m_dropna.js"),
+    f_dropna: require("./f_dropna.js"),
 }
