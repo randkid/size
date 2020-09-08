4e654239e9e91906632a7b127372db8424058136
Gnlow <entrydark@gmail.com>
2020-09-08 13:47:01 +0900

    load gender

--- mod.ts
+++ mod.ts
@@ -1,17 +1,21 @@
 import { PCA } from "https://raw.githubusercontent.com/gnlow/deno-pca/master/mod.ts"
 import { NumericalTuple } from "https://raw.githubusercontent.com/randkid/Randkid/master/mod.ts"
-import birthdate from "https://raw.githubusercontent.com/randkid/birthdate/master/mod.ts"
+import {
+    birthdate,
+    gender,
+} from "https://raw.githubusercontent.com/randkid/Collection/master/mod.ts"
 import { gaussian } from "https://deno.land/x/gaussian/mod.ts"
 import { hash } from "https://raw.githubusercontent.com/gnlow/planter/master/mod.ts"
 import f from "./src/f_model.js"
 import m from "./src/m_model.js"
 
 const a = new NumericalTuple({
-    inputMaterials: [birthdate],
+    inputMaterials: [birthdate, gender],
     ranges: [],
-    rand(seed, birthDateVal) {
+    rand(seed, birthDateVal, genderVal) {
         const date = new Date(birthDateVal)
-        const pca = PCA.load(f[2013 - date.getFullYear()].model)
+        const models = genderVal == "f" ? f : m
+        const pca = PCA.load(models[2013 - date.getFullYear()].model)
         const randomized = pca.getStandardDeviations().splice(0, 5)
             .map((σ: number, i: number) => 
                 gaussian(0, σ**2).ppf(hash(seed, i))
